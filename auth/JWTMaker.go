2402b09e04aea38ca67ab0f1ef0344f5c70c1fee
Masoud Heydari <m.heydari4883@gmail.com>
2022-01-28 17:02:43 +0330

    delete some funtions from JWTMaker

--- auth/JWTMaker.go
+++ auth/JWTMaker.go
@@ -1,15 +1,9 @@
 package auth
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -26,81 +20,6 @@ func NewJWtMaker(secretKey string) (TokenMaker, error) {
 	}
 
 	return &JWTMaker{secretKey}, nil
-}
-
-func (jwtMaker *JWTMaker) CreateToken(userId uint32) (string, error) {
-
-	claims := jwt.MapClaims{}
-	claims["authorized"] = true
-	claims["user_id"] = userId
-	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(os.Getenv("API_SECRET")))
-
-}
-
-func (jwtMaker *JWTMaker) VerifyToken(r *http.Request) error {
-	tokenString := jwtMaker.ExtractToken(r)
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(os.Getenv("API_SECRET")), nil
-	})
-	if err != nil {
-		return err
-	}
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		Pretty(claims)
-	}
-	return nil
-}
-
-func (jwtMaker *JWTMaker) ExtractToken(r *http.Request) string {
-	keys := r.URL.Query()
-	token := keys.Get("token")
-	if token != "" {
-		return token
-	}
-	bearerToken := r.Header.Get("Authorization")
-	if len(strings.Split(bearerToken, " ")) == 2 {
-		return strings.Split(bearerToken, " ")[1]
-	}
-	return ""
-}
-
-func (jwtMaker *JWTMaker) ExtractTokenID(r *http.Request) (uint32, error) {
-
-	tokenString := jwtMaker.ExtractToken(r)
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(os.Getenv("API_SECRET")), nil
-	})
-	if err != nil {
-		return 0, err
-	}
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if ok && token.Valid {
-		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
-		if err != nil {
-			return 0, err
-		}
-		return uint32(uid), nil
-	}
-	return 0, nil
-}
-
-//Pretty display the claims nicely in the terminal
-func Pretty(data interface{}) {
-	b, err := json.MarshalIndent(data, "", " ")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	fmt.Println(string(b))
 }
 
 // CreateTokenPayload creates a new token for a specific username and duration
--- auth/tokenMaker.go
+++ auth/tokenMaker.go
@@ -1,18 +1,11 @@
 package auth
 
 import (
-	"net/http"
 	"time"
 )
 
 // TokenMaker is an interface for managing tokens
 type TokenMaker interface {
-	VerifyToken(r *http.Request) error
-	CreateToken(userIid uint32) (string, error)
-	ExtractToken(r *http.Request) string
-	ExtractTokenID(r *http.Request) (uint32, error)
-
-	// payload
 	CreateTokenPayload(email string, duration time.Duration) (string, error)
 	VerifyTokenPayload(token string) (*Payload, error)
 }
